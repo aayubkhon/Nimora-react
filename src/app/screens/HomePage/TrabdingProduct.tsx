cb07d7720a5f5d0fc31a38585169d9728af385b5
aayubxon701 <aayubxon701@gmail.com>
2025-06-28 23:34:39 +0900

    tradingProduct is fixed new

--- src/app/screens/HomePage/TrabdingProduct.tsx
+++ src/app/screens/HomePage/TrabdingProduct.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { CardContent, Container, Stack } from "@mui/material";
+import {Container,Stack } from "@mui/material";
 import AspectRatio from "@mui/joy/AspectRatio";
-import Avatar from "@mui/joy/Avatar";
 import Box from "@mui/joy/Box";
 import Button from "@mui/joy/Button";
 import Card from "@mui/joy/Card";
 import CardCover from "@mui/joy/CardCover";
-import Chip from "@mui/joy/Chip";
 import IconButton from "@mui/joy/IconButton";
 import Typography from "@mui/joy/Typography";
 import Link from "@mui/joy/Link";
 import Favorite from "@mui/icons-material/Favorite";
 import Visibility from "@mui/icons-material/Visibility";
-import CreateNewFolder from "@mui/icons-material/CreateNewFolder";
-import CardOverflow from "@mui/joy/CardOverflow";
 import { CssVarsProvider } from "@mui/joy/styles";
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { Navigation,Pagination } from 'swiper/modules';
+
 
 const TrabdingProduct = () => {
   return (
@@ -26,53 +28,380 @@ const TrabdingProduct = () => {
           <Box className="frame_name"> TOP 15 TRENDING PRODUCTS</Box>
         </Stack>
         <CssVarsProvider>
-          <Card
-         
-            color="primary"
-            variant="plain"
-            sx={{
-              width: "340px",
-              height: "420px",
-              borderRadius: 0,
-              background: "rgb(247, 245, 239)",
-              cursor: "pointer",
-              
-            }}
-          >
-            <CardContent  className="Card">
-              <div className="wrap">
-                <CardCover className="ring">
-                  <img className="birst" src="/home/birstone.jpeg" alt="" />
-                </CardCover>
-                <CardOverflow
-                  className="crs"
-                  sx={{ marginTop: 45, marginLeft: 25, width: 100 }}
-                >
-                  <Button
-                    color="neutral"
-                    onClick={function () {}}
-                    size="sm"
-                    variant="plain"
-                    className="Add_cart"
+   <Swiper  modules={[Pagination]}
+      spaceBetween={40}
+      grabCursor
+      slidesPerView={3}
+      slideToClickedSlide
+      navigation={false}
+      pagination={{ clickable: true }}
+      
+     >
+    <SwiperSlide>
+             <Card variant="plain" sx={{ width: 400, bgcolor: "initial", p: 0 }}>
+            <Box sx={{ position: "relative" }}>
+              <AspectRatio ratio="4/5">
+                <figure>
+                  <img src="/home/best.jpeg" alt="Yosemite by Casey Horner" />
+                </figure>
+              </AspectRatio>
+
+              <CardCover
+                className="gradient-cover"
+                sx={{
+                  "&:hover, &:focus-within": {
+                    opacity: 1,
+                  },
+                  opacity: 0,
+                  transition: "0.1s ease-in",
+                  cursor:'pointer',
+                }}
+              >
+                <div>
+                  <Box
+                  className="add_box"
+                    sx={{
+                      p: 2,
+                      display: "flex",
+                      alignItems: "center",
+                      gap: 1.5,
+                      flexGrow: 1,
+                      alignSelf: "flex-end",
+                    }}
                   >
-                    Add to bag
-                  </Button>
-                </CardOverflow>
-              </div>
-              <CardOverflow>
-                {/* <IconButton
-                  sx={{
-                    position: "absolute",
-                    zIndex: 2,
-                    bottom: 30,
-                    transform: "translateY(50%)",
-                  }}
-                >
-                  <Favorite />
-                </IconButton> */}
-              </CardOverflow>
-            </CardContent>
+                    <IconButton
+                      size="sm"
+                      variant="solid"
+                      color="danger"
+                      sx={{ bgcolor: "rgba(0 0 0 / 0.2)" }}
+                    >
+                      <Favorite />
+                    </IconButton>
+                      <Button
+  
+                        className="add_bag"
+                      >
+                        Add to bag
+                      </Button>
+                  </Box>
+                </div>
+              </CardCover>
+            </Box>
+        <Box flexDirection={"column"}>
+              <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+              <Typography  className="product_name">
+                Faded Grandeur Stud Earrings
+              </Typography>
+              
+
+              <Link
+                href="#dribbble-shot"
+                level="body-xs"
+                underline="none"
+                startDecorator={<Favorite />}
+                sx={{
+                  fontWeight: "md",
+                  ml: "auto",
+                  color: "text.secondary",
+                  "&:hover": { color: "danger.plainColor" },
+                }}
+              >
+                117
+              </Link>
+              <Link
+                href="#dribbble-shot"
+                level="body-xs"
+                underline="none"
+                startDecorator={<Visibility />}
+                sx={{
+                  fontWeight: "md",
+                  color: "text.secondary",
+                  "&:hover": { color: "primary.plainColor" },
+                }}
+              >
+                10.4k
+              </Link>
+            </Box>
+          <Typography className="product_price">
+                From: $52.00
+              </Typography>
+        </Box>
           </Card>
+    </SwiperSlide>
+      <SwiperSlide>
+             <Card variant="plain" sx={{ width: 400, bgcolor: "initial", p: 0 }}>
+            <Box sx={{ position: "relative" }}>
+              <AspectRatio ratio="4/5">
+                <figure>
+                  <img src="/home/best.jpeg" alt="Yosemite by Casey Horner" />
+                </figure>
+              </AspectRatio>
+
+              <CardCover
+                className="gradient-cover"
+                sx={{
+                  "&:hover, &:focus-within": {
+                    opacity: 1,
+                  },
+                  opacity: 0,
+                  transition: "0.1s ease-in",
+                  cursor:'pointer',
+                }}
+              >
+                <div>
+                  <Box
+                  className="add_box"
+                    sx={{
+                      p: 2,
+                      display: "flex",
+                      alignItems: "center",
+                      gap: 1.5,
+                      flexGrow: 1,
+                      alignSelf: "flex-end",
+                    }}
+                  >
+                    <IconButton
+                      size="sm"
+                      variant="solid"
+                      color="danger"
+                      sx={{ bgcolor: "rgba(0 0 0 / 0.2)" }}
+                    >
+                      <Favorite />
+                    </IconButton>
+                      <Button
+  
+                        className="add_bag"
+                      >
+                        Add to bag
+                      </Button>
+                  </Box>
+                </div>
+              </CardCover>
+            </Box>
+        <Box flexDirection={"column"}>
+              <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+              <Typography  className="product_name">
+                Faded Grandeur Stud Earrings
+              </Typography>
+              
+
+              <Link
+                href="#dribbble-shot"
+                level="body-xs"
+                underline="none"
+                startDecorator={<Favorite />}
+                sx={{
+                  fontWeight: "md",
+                  ml: "auto",
+                  color: "text.secondary",
+                  "&:hover": { color: "danger.plainColor" },
+                }}
+              >
+                117
+              </Link>
+              <Link
+                href="#dribbble-shot"
+                level="body-xs"
+                underline="none"
+                startDecorator={<Visibility />}
+                sx={{
+                  fontWeight: "md",
+                  color: "text.secondary",
+                  "&:hover": { color: "primary.plainColor" },
+                }}
+              >
+                10.4k
+              </Link>
+            </Box>
+          <Typography className="product_price">
+                From: $52.00
+              </Typography>
+        </Box>
+          </Card>
+    </SwiperSlide>
+      <SwiperSlide>
+             <Card variant="plain" sx={{ width: 400, bgcolor: "initial", p: 0 }}>
+            <Box sx={{ position: "relative" }}>
+              <AspectRatio ratio="4/5">
+                <figure>
+                  <img src="/home/best.jpeg" alt="Yosemite by Casey Horner" />
+                </figure>
+              </AspectRatio>
+
+              <CardCover
+                className="gradient-cover"
+                sx={{
+                  "&:hover, &:focus-within": {
+                    opacity: 1,
+                  },
+                  opacity: 0,
+                  transition: "0.1s ease-in",
+                  cursor:'pointer',
+                }}
+              >
+                <div>
+                  <Box
+                  className="add_box"
+                    sx={{
+                      p: 2,
+                      display: "flex",
+                      alignItems: "center",
+                      gap: 1.5,
+                      flexGrow: 1,
+                      alignSelf: "flex-end",
+                    }}
+                  >
+                    <IconButton
+                      size="sm"
+                      variant="solid"
+                      color="danger"
+                      sx={{ bgcolor: "rgba(0 0 0 / 0.2)" }}
+                    >
+                      <Favorite />
+                    </IconButton>
+                      <Button
+  
+                        className="add_bag"
+                      >
+                        Add to bag
+                      </Button>
+                  </Box>
+                </div>
+              </CardCover>
+            </Box>
+        <Box flexDirection={"column"}>
+              <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+              <Typography  className="product_name">
+                Faded Grandeur Stud Earrings
+              </Typography>
+              
+
+              <Link
+                href="#dribbble-shot"
+                level="body-xs"
+                underline="none"
+                startDecorator={<Favorite />}
+                sx={{
+                  fontWeight: "md",
+                  ml: "auto",
+                  color: "text.secondary",
+                  "&:hover": { color: "danger.plainColor" },
+                }}
+              >
+                117
+              </Link>
+              <Link
+                href="#dribbble-shot"
+                level="body-xs"
+                underline="none"
+                startDecorator={<Visibility />}
+                sx={{
+                  fontWeight: "md",
+                  color: "text.secondary",
+                  "&:hover": { color: "primary.plainColor" },
+                }}
+              >
+                10.4k
+              </Link>
+            </Box>
+          <Typography className="product_price">
+                From: $52.00
+              </Typography>
+        </Box>
+          </Card>
+    </SwiperSlide>
+     <SwiperSlide>
+             <Card variant="plain" sx={{ width: 400, bgcolor: "initial", p: 0 }}>
+            <Box sx={{ position: "relative" }}>
+              <AspectRatio ratio="4/5">
+                <figure>
+                  <img src="/home/best.jpeg" alt="Yosemite by Casey Horner" />
+                </figure>
+              </AspectRatio>
+
+              <CardCover
+                className="gradient-cover"
+                sx={{
+                  "&:hover, &:focus-within": {
+                    opacity: 1,
+                  },
+                  opacity: 0,
+                  transition: "0.1s ease-in",
+                  cursor:'pointer',
+                }}
+              >
+                <div>
+                  <Box
+                  className="add_box"
+                    sx={{
+                      p: 2,
+                      display: "flex",
+                      alignItems: "center",
+                      gap: 1.5,
+                      flexGrow: 1,
+                      alignSelf: "flex-end",
+                    }}
+                  >
+                    <IconButton
+                      size="sm"
+                      variant="solid"
+                      color="danger"
+                      sx={{ bgcolor: "rgba(0 0 0 / 0.2)" }}
+                    >
+                      <Favorite />
+                    </IconButton>
+                      <Button
+  
+                        className="add_bag"
+                      >
+                        Add to bag
+                      </Button>
+                  </Box>
+                </div>
+              </CardCover>
+            </Box>
+        <Box flexDirection={"column"}>
+              <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+              <Typography  className="product_name">
+                Faded Grandeur Stud Earrings
+              </Typography>
+              
+
+              <Link
+                href="#dribbble-shot"
+                level="body-xs"
+                underline="none"
+                startDecorator={<Favorite />}
+                sx={{
+                  fontWeight: "md",
+                  ml: "auto",
+                  color: "text.secondary",
+                  "&:hover": { color: "danger.plainColor" },
+                }}
+              >
+                117
+              </Link>
+              <Link
+                href="#dribbble-shot"
+                level="body-xs"
+                underline="none"
+                startDecorator={<Visibility />}
+                sx={{
+                  fontWeight: "md",
+                  color: "text.secondary",
+                  "&:hover": { color: "primary.plainColor" },
+                }}
+              >
+                10.4k
+              </Link>
+            </Box>
+          <Typography className="product_price">
+                From: $52.00
+              </Typography>
+        </Box>
+          </Card>
+    </SwiperSlide>
+   </Swiper>
         </CssVarsProvider>
       </Container>
     </div>
