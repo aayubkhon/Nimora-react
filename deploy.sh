7eb2a3e8bc4113e6f0879c11becaab30acb469bc
aayubxon701 <aayubxon701@gmail.com>
2026-04-02 02:00:08 +0900

    feat: deploy nimora client on production

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# PRODUCTION
+git reset --hard
+git pull origin master
+
+nmp i yarn -g
+yarn
+yarn run build
+pm2 start "yarn run start:prod" --name=NIMORA-REACT
+
+# DEVELOPMENT
+# npm i yarn -g
+# yarn
+# pm2 start "yarn run start" --name=NIMORA-REACT
